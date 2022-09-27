4eb4f741229cbb56eaeff2c1e362e73eb3fdb8f8
subha287 <112364114+subha287@users.noreply.github.com>
2022-09-27 17:20:31 -0400

    g
    g

--- solution-hw3/app.js
+++ solution-hw3/app.js
@@ -1,23 +1,49 @@
-glazingprice_list = {
-  keep_original: 0.0,
-  sugar_milk: 0.0,
-  vanilla_milk: 0.5,
-  double_chocolate: 1.5,
-};
- 
 
-// first function to change price when glazing option changes, second funtion is for when pack size changes.
+
+let allGlazes = [["Keep Original",0], ["Sugar Milk",0], ["Vanilla Milk",0.5], ["Double Chocolate",1.5]];
+
+let allPacks = [1,3,6,12]
+
+var dropdown1 = document.getElementById("glazingOptions");
+
+for (i = 0; i < allGlazes.length; i++) {
+   var opt = document.createElement("option");
+   opt.text = allGlazes[i][0];
+   opt.value = allGlazes[i][1];
+   dropdown1.options.add(opt);
+}
+
+var dropdown2 = document.getElementById("pack");
+
+for (i = 0; i < allPacks.length; i++) {
+   var opt = document.createElement("option");
+   opt.text = allPacks[i];
+   opt.value = allPacks[i];
+   dropdown2.options.add(opt);
+}
+
+
+// first function to change price when glazing option changes, second function is for when pack size changes.
 function glazingChange(glazingOption) {
-  var value = glazingOption.value;
-  pack = document.getElementById("pack").value;
+  var value = parseFloat(glazingOption.value);
+  p=document.getElementById("pack").value;
+  console.log(value)
+  console.log("$" + (2.49 + value).toFixed(2).toString())
+
   document.getElementById("price").innerHTML =
-    "$" + ((2.49 + glazingprice_list[value]) * pack).toFixed(2).toString();
+    "$" + ((2.49 + value)*p).toFixed(2).toString();
 }
 
 function packSizeChange(packSizeOption) {
-  var value = packSizeOption.value;
-  glazingprice =
-    glazingprice_list[document.getElementById("glazingOptions").value];
+  var pack = parseInt(packSizeOption.value);
+  glazingprice = parseFloat(document.getElementById("glazingOptions").value);
+  console.log("p",pack)
+  console.log("gp",glazingprice)
   document.getElementById("price").innerHTML =
-    "$" + ((2.49 + glazingprice) * value).toFixed(2).toString();
+      "$" + ((2.49 + glazingprice) * pack).toFixed(2).toString();
 }
+
+
+
+
+
